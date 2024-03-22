8ffd2e5e34f9edaf7a6066e0035ac9ca4ba32229
Tova Rottenstein <101480321+tovatovatova@users.noreply.github.com>
2024-03-22 14:23:55 +0200

    Initial commit

---
+++ Hadasim-COVID19/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Hadasim_COVID19.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
